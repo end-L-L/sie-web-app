39baa81c3ef0e0a011697ba132cd070dca745398
Boset <boss-x86@protonmail.com>
2024-04-22 15:59:06 -0600

    update graficas

--- src/app/screens/graficas-screen/graficas-screen.component.ts
+++ src/app/screens/graficas-screen/graficas-screen.component.ts
@@ -1,6 +1,8 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import DatalabelsPlugin from 'chartjs-plugin-datalabels';
 import { AdminService } from 'src/app/services/admin.service';
+import { FacadeService } from 'src/app/services/facade.service';
 
 interface ChartData {
   labels: string[];
@@ -22,40 +24,74 @@ interface ChartDataset {
 export class GraficasScreenComponent implements OnInit {
 
   public data:any = {};
+  public token:string = '';
 
   constructor(
+    private facadeService: FacadeService,
     private adminServices: AdminService,
-    private cdr: ChangeDetectorRef
+    private cdr: ChangeDetectorRef,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
+
+    this.token = this.facadeService.getSessionToken();
+
+    if (this.token === '') {
+      this.router.navigate(['']);
+    }
+
     this.obtenerTotalUsuarios();
   }
 
-  // Bar Chart
-  barChartData: ChartData = {
-    labels: ['Administradores', 'Maestros', 'Alumnos'],
+  // Line Chart
+  lineChartData: ChartData ={
+    labels: ['Administradores', 'Alumnos', 'Maestros'],
     datasets: [
       {
-        data: [], // This will hold dynamic data for user counts
+        data:[],
         label: 'Cantidad de Usuarios',
-        backgroundColor: ['#007bff', '#28a745', '#dc3545'],
+        backgroundColor: [],
+      },
+    ],
+  };
+
+  lineChartOptions = {
+    responsive:false,
+    maintainAspectRatio: false,
+  }
+
+  lineChartPlugins = [ DatalabelsPlugin ];
+
+  // Bar Chart
+  barChartData: ChartData = {
+    labels:  ['Administradores', 'Alumnos', 'Maestros'],
+    datasets: [
+      {
+        data: [], // Dynamic Data
+        label: 'Cantidad de Usuarios',
+        backgroundColor: [
+        'rgba(255, 99, 132, 0.4)',
+        'rgba(200, 100, 255, 0.6)',
+        'rgba(40, 180, 210, 0.8)',
+        ],
       },
     ],
   };
 
   barChartOptions = {
     responsive: true,
+    maintainAspectRatio: false,
   };
 
   barChartPlugins = [DatalabelsPlugin];
 
   // Pie Chart
   pieChartData: ChartData = {
-    labels: ['Administradores', 'Maestros', 'Alumnos'],
+    labels:  ['Administradores', 'Alumnos', 'Maestros'],
     datasets: [
       {
-        data: [], // Datos dinámicos irán aquí
+        data: [], // Dynamic Data
         label: 'Registro de usuarios',
         backgroundColor: ['#FCFF44', '#F1C8F2', '#31E731'],
       },
@@ -64,50 +100,56 @@ export class GraficasScreenComponent implements OnInit {
 
   pieChartOption = {
     responsive: true,
+    maintainAspectRatio: false,
   };
 
   pieChartPlugins = [DatalabelsPlugin];
 
-  // Doughnut Chart
-  doughnutChartData: ChartData = {
-    labels: ['Administradores', 'Maestros', 'Alumnos'],
+  // Polar Area Chart
+  polarAreaChartData: ChartData = {
+    labels:  ['Administradores', 'Alumnos', 'Maestros'],
     datasets: [
       {
-        data: [], // Datos dinámicos irán aquí
+        data: [], // Dynamic Data
         label: 'Registro de usuarios',
         backgroundColor: ['#F88406', '#FCFF44', '#31E7E7'],
       },
     ],
   };
 
-  doughnutChartOption = {
+  polarAreaChartOption = {
     responsive: true,
+    maintainAspectRatio: false,
   };
 
-  doughnutChartPlugins = [DatalabelsPlugin];
+  polarAreaChartPlugins = [DatalabelsPlugin];
 
   obtenerTotalUsuarios() {
     this.adminServices.getTotalUsuarios().subscribe({
       next: (data) => {
-        console.log('Datos recibidos:', data);
+        console.log('Datos:', data);
         this.updateChartData(data);
         this.cdr.detectChanges();
       },
       error: (error) => {
-        console.error('Error al obtener datos de usuarios', error);
+        console.error('Error al Obtener Datos', error);
       },
     });
   }
 
   updateChartData(data: any) {
-    this.pieChartData.datasets[0].data = [data.admins, data.maestros, data.alumnos];
-    this.doughnutChartData.datasets[0].data = [data.admins, data.maestros, data.alumnos];
-    this.barChartData.datasets[0].data = [data.admins, data.maestros, data.alumnos];
+    // Actualizar Datos de Gráficos 
+    this.lineChartData.datasets[0].data = [data.admins, data.alumnos, data.maestros];
+    this.barChartData.datasets[0].data = [data.admins, data.alumnos, data.maestros];
+    this.pieChartData.datasets[0].data = [data.admins, data.alumnos, data.maestros];
+    this.polarAreaChartData.datasets[0].data = [data.admins, data.alumnos, data.maestros];
 
     // Reasignar Datos para Activar Actualizaciones de Gráficos
-    this.pieChartData = { ...this.pieChartData };
-    this.doughnutChartData = { ...this.doughnutChartData };
+    this.lineChartData = { ...this.lineChartData };
     this.barChartData = { ...this.barChartData };
+    this.pieChartData = { ...this.pieChartData };
+    this.polarAreaChartData = { ...this.polarAreaChartData };
+    
     this.cdr.detectChanges();
   }
 }
